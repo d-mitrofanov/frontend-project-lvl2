881ef421479f75c56151fb34e27c31013711cc01
root <root@DESKTOP-S45I43D.localdomain>
2020-07-19 15:31:55 +0300

    Second step

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,5 +4,6 @@ import program from 'commander';
 
 program
   .version('0.0.1')
+  .option('-f, --format [type]', 'output format')
   .description('Find difference between files')
   .parse(process.argv);
