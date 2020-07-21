78205dde51421348a084919dd914284f533f4b0d
root <root@DESKTOP-S45I43D.localdomain>
2020-07-21 21:33:39 +0300

    asciinema (step3) + getignore and eslintrc

---
+++ .eslintrc.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2020: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  rules: {
+    'no-console': 'off',
+    'import/extensions': 0,
+  },
+};
---
+++ README.md
@@ -0,0 +1,2 @@
+Step 3
+[![asciicast](https://asciinema.org/a/Jt3UMTtRrUEeKjPHNh3pbtR9L.svg)](https://asciinema.org/a/Jt3UMTtRrUEeKjPHNh3pbtR9L)
